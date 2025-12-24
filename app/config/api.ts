66faebd2b4e384dfbff4c955ea9579117a16f3c0
issc-Swapnil <patilswapnil8956@gmail.com>
2025-12-24 12:34:44 +0530

    new url added

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,3 +1,3 @@
 // const BASE_URL = "https://mba-college-api-zgzx.onrender.com"
-const BASE_URL = "https://mba-college-api-production.up.railway.app"
+const BASE_URL = "https://mba-college-api-production-a320.up.railway.app"
 export default BASE_URL;
